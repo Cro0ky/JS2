619ed4059ea5b10c24628bf1a1d7959f1b3befc0
Cro0ky <143035135+Cro0ky@users.noreply.github.com>
2024-01-23 15:20:21 +0300

    Add files via upload

---
+++ JsIfSwitch/cikzi.js
@@ -0,0 +1,113 @@
+// let a = 5
+// while (a < 10) {
+//     console.log(a);
+//     a++  
+// }
+
+// while(a) {
+//     console.log(a);
+//     a--
+// }
+
+// do {
+//     console.log(a);
+//     a--
+// } while (a>=0) 
+
+// let i = 0
+// for (i; i<10; i++) {
+//     console.log(i);
+// }
+// console.log(i)
+
+// sl = [1,2,3,4,5,6,7]
+
+// for (let item in sl) {
+//     console.log(item);
+// }
+
+// firstFor : for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j < 5; j++) {
+//         if (j == 3) {
+//             break firstFor;
+//         }
+//         console.log(i, j);
+//     }
+// }
+
+// function summ(a, b) {
+//     let first, second;
+//     function one(x) {
+//         first = x
+//     }
+//     function two(y) {
+//         second = y
+//     }
+//     one(15)
+//     two(2)
+//     return first + second
+// }
+// console.log(summ());
+
+// function one (x=10) {
+//     console.log(x);
+// }
+
+// function two(x=15) {
+//     console.log(x);
+// }
+
+// function calc(x, y, first, second) {
+//     if (x > y){
+//         first(100)
+//     } else {
+//         second(200)
+//     }
+// }
+// calc(6, 2, one, two)
+
+// let a = one 
+
+// a(52)
+
+// function less (a, b) {
+//     if (a < b) {
+//         return a
+//     }
+//     return b
+// }
+// console.log(less(20, 6));
+
+// let showMessage = function () {
+//     console.log('message');
+// }
+// function showMessage2 () {
+//     console.log('message2');
+// }
+// const showMessage3 = () => {
+//     console.log('message3');
+// }
+
+// const showMessage4 = (a) => {
+//     console.log(a);
+// }
+
+// const showMessage5 = function (name, text, i ) {
+//     console.log(`${name},${text}`);
+//     if (i === 5) {
+//         clearInterval(timer)
+//     }
+//     ++i
+//     console.log(i);
+// }
+
+// showMessage()
+// showMessage2()
+// showMessage3()
+// showMessage4('message4')
+// showMessage5('user1','message5')
+// setTimeout(showMessage5, 1000, 'user12','message5', 1)
+// let timer = setInterval(showMessage5, 2000, 'user1','message5', 1)
+
+
+
---
+++ JsIfSwitch/main.js
@@ -0,0 +1,199 @@
+// let a = 5 < 9 ? console.log(15) : console.log(14);
+
+// let b = (3<4) ? true : (2<4) ? 15 : 'error'
+
+// let a = 24, b = 33
+
+// let (a > 15 && b < 100) {
+//     console.log('aaa')
+// } 
+// console.log('qqq');
+
+// let state = true
+// console.log(state && 'asdasd');
+
+// let a = 1
+// switch (a) {
+//     case 1:
+//         console.log('123123');
+//         break
+//     case 2:
+//         console.log('234234');
+//         break
+//     default:
+//         console.log('345345');
+//         break
+// }
+
+// let lang = 1;
+
+// rus = ['пн','вт','ср','чт','пт','сб','вс',]
+// eng = ['mon','tu','wen','th','fi','st','sd',]
+
+// if (lang !== 1) {
+//     console.log(rus);
+// } else {
+//     console.log(eng);
+// } 
+
+
+//1 
+// let min = 32
+
+// if (0 <= min && min <15 ) {
+//     console.log(1);
+// } else if (min < 30) {
+//     console.log(2);
+// } else if (min < 45) {
+//     console.log(3);
+// } else if (min < 60) {
+//     console.log(4);
+// } else {
+//     console.log('Число не в диапозоне от 0 до 59');
+// }
+
+//2
+// let a = 0
+
+// if (a === 0 ) {
+//     console.log('fine');  
+// } else {
+//     console.log('error');
+// }
+
+// if (a > 0) {
+//     console.log('fine');
+// } else {
+//     console.log('error');
+// }
+
+// if (a < 0) {
+//     console.log('fine');
+// } else {
+//     console.log('error');
+// }
+
+// if (a >= 0) {
+//     console.log('fine');
+// } else {
+//     console.log('error');
+// }
+
+// if (a <= 0) {
+//     console.log('fine');
+// } else {
+//     console.log('error');
+// }
+
+// if (a !== 0 ) {
+//     console.log('fine');  
+// } else {
+//     console.log('error');
+// }
+
+// if (a === 'test' ) {
+//     console.log('fine');  
+// } else {
+//     console.log('error');
+// }
+
+// if (a === '1' ) {
+//     console.log('fine');  
+// } else {
+//     console.log('error');
+// }
+
+//3 
+// let a = 0
+// let b = 0
+// if (a > 0 && a < 5) {
+//     console.log('fine');
+// } else {
+//     console.log('error');
+// }
+
+// if (a == 2 || a == 0) {
+//     a += 7
+// } else {
+//     a /= 10
+//     alert(a)
+// }
+
+// if (a <= 1 && b >= 3 ) {
+//     console.log(a + b);
+// } else {
+//     console.log(a - b);
+// }
+
+
+// if (a > 2 && a < 11 || b >= 6 && b < 14) {
+//     console.log('fine');
+// } else {
+//     console.log('error');
+// }
+
+//4 
+// let num = 1
+// switch (a) {
+//     case 1:
+//         console.log('Зима');
+//         break
+//     case 2:
+//         console.log('весна');
+//         break
+//     case 3:
+//         console.log('лето');
+//         break
+//     case 4:
+//         console.log('осень');
+//         break
+//     default:
+//         console.log('error');
+//         break
+// }
+
+//5
+// let day = 2
+// if (day <= 1 && day <= 10) {
+//     console.log(1);
+// } else if (day <= 11 && day <= 20) {
+//     console.log(2);
+// } else if (day <= 21 && day <= 31){
+//     console.log(3);
+// } else {
+//     console.log('error');
+// }
+
+//6
+// let month = 2
+// if (month <= 12 && month <= 2) {
+//     console.log("зима");
+// } else if (month <= 3 && month <= 5) {
+//     console.log("весна");
+// } else if (month <= 6 && month <= 8){
+//     console.log("лето");
+// } else if (month <= 9 && month <= 11){
+//     console.log("осень");
+// } else {
+//     console.log('error');
+// }
+
+//7 
+// let str = 'abcde'
+// if (str[0] === 'a') {
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
+
+//8
+// let num = '123'
+// console.log(Number(num[0]) + Number(num[1]) + Number(num[2]));
+
+//9
+// let num = '123123'
+// if (Number(num[0]) + Number(num[1]) + Number(num[2]) === Number(num[3]) + Number(num[5]) + Number(num[4])) {
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
